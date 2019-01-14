02e005c0c83ef192fbf1dc840570acbf1383e9ff
Анна Колесникова <a.kolesnikova@reg.ru>
2019-01-14 16:58:44 +1000

    Исправила ошибки в коде по трэвису

--- js/data.js
+++ js/data.js
@@ -3,9 +3,11 @@
 'use strict';
 (function () {
   window.onError = function (message) {
+    // eslint-disable-next-line no-console
     console.error(message);
   };
   window.onLoad = function (data) {
+    // eslint-disable-next-line no-console
     console.log(data);
   };
   window.load();
